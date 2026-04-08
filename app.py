2ade5c2056e2d53f317510d81f2255856a1e0170
Mukil <mukil@gmail.com>
2026-04-08 22:50:53 +0530

    Fix missing FastAPI imports in app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, FileResponse, PlainTextResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
